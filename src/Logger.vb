5a0d22cb79ce41ebb11c70399ed9a2f8592083ee
peach <skpikao233@gmail.com>
2018-02-08 19:12:29 +0000

    Add src and example

---
+++ src/Logger.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+
+Public Class Logger
+
+    Dim path As String
+
+    Function Logger(Optional logfilePath As String = "logFile.log")
+        path = logfilePath
+    End Function
+
+    Public Enum LogType
+        Message
+        Warning
+        VariableDump
+    End Enum
+
+    Function LogToFile(logType As LogType, message As String, Optional description As String = "") As Task
+        Select Case logType
+            Case LogType.Message
+                message = String.Concat(DateTime.Now.ToLongTimeString, ":    ", "Message", "    ", description, "   ", message)
+            Case LogType.Warning
+                message = String.Concat(DateTime.Now.ToLongTimeString, ":    ", "Warning", "    ", description, "   ", message)
+            Case LogType.VariableDump
+                message = String.Concat(DateTime.Now.ToLongTimeString, ":    ", "Variable", "    ", description, "   ", message)
+        End Select
+        File.AppendAllText(path, message + vbCrLf)
+    End Function
+
+
+
+
+End Class
---
+++ src/Module1.vb
@@ -0,0 +1,31 @@
+﻿Imports System
+
+Module Module1
+    Sub Main()
+        someShittySub()
+    End Sub
+
+    Sub someShittySub()
+        Console.Title = "some cheap ass logger"
+        Dim userInput As String
+        Dim logFile As Logger = New Logger()
+        logFile.Logger("myLog.log")
+        While True
+            Console.WriteLine("prefixes = warning, msg, var")
+            userInput = Console.ReadLine()
+            If (userInput.Contains("warning")) Then
+                logFile.LogToFile(Logger.LogType.Warning, userInput, "description")
+            ElseIf (userInput.Contains("msg")) Then
+                logFile.LogToFile(Logger.LogType.Message, userInput, "description")
+            ElseIf (userInput.Contains("var")) Then
+                logFile.LogToFile(Logger.LogType.VariableDump, userInput, "description")
+            Else
+                Console.WriteLine("you didn't include any keywords...")
+            End If
+
+        End While
+    End Sub
+
+
+
+End Module
